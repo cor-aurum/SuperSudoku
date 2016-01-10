fe21232878044b763463bb5aa103a4671e469dcd
felix <main@felix-schuetze.de>
2016-01-10 12:29:03 +0100

    Da gibt es eine ganze Menge Syntaxfehler wegen Mehrfachdefinition von Funktionen. Wir sind leider nicht in Java, aber wie testest du das so?

---
+++ Sudoku/src/GUI.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * SUDOKU ./reader.c                                                          *
+ * Steuerung für die Händische Manipulation von Sudokus.
+ * @author Felix Schütze, dhbw@felix-schuetze.de
+ ******************************************************************************/
+
+
+
+/* EXTERNE HEADER-DATEIEN *****************************************************/
+#include<stdio.h>
+#include<stdlib.h>
+
+/* PRÄPROZESSORDIREKTIVEN *****************************************************/
+#define HOEHE 9
+#define BREITE 9
+#define KACHELHOEHE 3
+#define KACHELBREITE 3
+#define MAX_ZAHL 9
+
+int main()
+{
+	printf("Steuerung");
+	return 0;
+}
