1b7afd784d12fff9006581598e49a4429b9cdd9b
felix <main@felix-schuetze.de>
2016-01-10 15:05:55 +0100

    meldungAusgaben(char*) funktioniert. GUI muss unbedingt unter Windows getestet werden!

--- Sudoku/src/GUI.h
+++ Sudoku/src/GUI.h
@@ -6,6 +6,7 @@
 #include <termios.h>
 void printFeld();
 int setFeld(int x, int y, int eingabe, int lock);
+int x = 3, y = 2;
 
 /*
  * Erhaelt ein char, ohne, dass man hierfür Enter drücken muss
@@ -23,12 +24,24 @@ int getch() {
 	return ch;
 }
 
+/*
+ * Verschiebt den Cursor nach x,y
+ */
 void gotoxy(int x, int y) {
 	printf("%c[%d;%df", 0x1B, y, x);
 }
 
+void meldungAusgeben(char* nachricht) {
+	gotoxy(1, HOEHE * 2+2);
+	printf(nachricht);
+	gotoxy(x, y);
+}
+
+/*
+ * Empfaengt Tastatureingaben und steuert mit diesen das Spielfeld
+ */
 int eingabeLoop() {
-	int x = 3, y = 2;
+
 	system("clear");
 	printFeld();
 	gotoxy(x, y);
@@ -37,25 +50,27 @@ int eingabeLoop() {
 		switch (tmp) {
 		case 'w':
 			if (y >= 4)
-				y-=2;
+				y -= 2;
 			break;
 		case 'a':
 			if (x >= 7)
-				x-=5;
+				x -= 5;
 			break;
 		case 's':
-			if (y < HOEHE*2)
-			y+=2;
+			if (y < HOEHE * 2)
+				y += 2;
 			break;
 		case 'd':
-			if (x < BREITE*5-2)
-			x+=5;
+			if (x < BREITE * 5 - 2)
+				x += 5;
 			break;
 		default:
 			if (tmp >= '1' && tmp <= '9') {
-				setFeld((y-2)/2,(x-3)/5,tmp-'0',0);
+				setFeld((y - 2) / 2, (x - 3) / 5, tmp - '0', 0);
 				system("clear");
 				printFeld();
+				if(tmp=='8')
+					meldungAusgeben("Es wurde eine Acht gedrückt");
 				//fflush(stdout);
 			}
 		}
